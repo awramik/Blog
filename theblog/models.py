c5ecb58a45fb95b744bb57e9f01d5c7c8f90482e
wikto <wikto@WIKTORIA>
2024-04-22 19:38:59 +0200

    updated models.py

--- theblog/models.py
+++ theblog/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_public = models.BooleanField(default=True)
+    password = models.CharField(max_length=50, blank=True, null=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    media_link = models.URLField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post.title}'
