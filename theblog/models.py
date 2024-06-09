50a1ffcdab49ff2a5346f7acc6cf7b491dd58675
wikto <wikto@WIKTORIA>
2024-06-09 15:01:56 +0200

    add blog post

--- theblog/models.py
+++ theblog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -17,6 +18,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
+
+    def get_absolute_url(self):
+        return reverse('article-detail', args=(str(self.id)) )
 
 
 class Comment(models.Model):
