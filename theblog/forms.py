98a9f2d5ab5c67b9910d270a51f4151bcb3e411a
wikto <wikto@WIKTORIA>
2024-06-09 17:25:51 +0200

    update and edit blog post

--- theblog/forms.py
+++ theblog/forms.py
@@ -10,7 +10,24 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title':forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter blog title...'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title tag for this post...'}),
-            'author': forms.SelectMultiple(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'media_link': forms.URLInput(attrs={'class': 'form-control'}),
+            'is_public': forms.CheckboxInput(attrs={'class': 'form-control'}),
+            'password': forms.PasswordInput(attrs={'class': 'form-control'}),
+
+        }
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'body', 'image', 'media_link', 'is_public', 'password')
+
+        widgets = {
+            'title':forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter blog title...'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title tag for this post...'}),
+            #'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
             'media_link': forms.URLInput(attrs={'class': 'form-control'}),
