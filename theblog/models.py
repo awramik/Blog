8d54c843d00b17724cce910515905fd513cc01c7
wikto <wikto@WIKTORIA>
2024-06-09 16:00:10 +0200

    add blog post 2

--- theblog/models.py
+++ theblog/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    title_tag = models.CharField(max_length=255, default="My Blog!")
+    title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
@@ -20,7 +20,8 @@ class Post(models.Model):
         return self.title + ' | ' + str(self.author)
 
     def get_absolute_url(self):
-        return reverse('article-detail', args=(str(self.id)) )
+        #return reverse('article-detail', args=(str(self.id)) )
+        return reverse('home')
 
 
 class Comment(models.Model):
