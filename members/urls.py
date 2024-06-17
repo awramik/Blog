7e0875933f9a3cc7965212c36a74712eb78c575f
wikto <wikto@WIKTORIA>
2024-06-17 22:36:39 +0200

    fixed logout and added blog category navbar links

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import UserRegisterView
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
-
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- theblog/views.py
+++ theblog/views.py
@@ -3,8 +3,6 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy
-
-
 # def home(request):
 #    return render(request, 'home.html', {})
 
@@ -15,11 +13,30 @@ class HomeView(ListView):
     ordering = ['-post_date']
     # ordering = ['-id']
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
+def CategoryListView(request):
+    cat_menu_list = Category.objects.all()
+    return render(request, 'category_list.html', {'cat_menu_list': cat_menu_list})
+
+def CategoryView(request, cats):
+    category_posts = Post.objects.filter(category=cats.replace('-', ' '))
+    return render(request, 'categories.html', {'cats': cats.title().replace('-', ' '), 'category_posts': category_posts})
 
 
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
 
 
 class AddPostView(CreateView):
@@ -37,19 +54,12 @@ class UpdatePostView(UpdateView):
 
 
 class AddCategoryView(CreateView):
-     model = Category
-     #form_class = PostForm
-     template_name = 'add_category.html'
-     fields = '__all__'
-     #fields = ('title', 'body')
+    model = Category
+    template_name = 'add_category.html'
+    fields = '__all__'
 
 
 class DeletePostView(DeleteView):
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('home')
-
-
-def CategoryView(request, cats):
-    category_posts = Post.objects.filter(category=cats.replace('-', ' '))
-    return render(request, 'categories.html', {'cats':cats.title().replace('-', ' '), 'category_posts': category_posts})
